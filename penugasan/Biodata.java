abc4de4fcf38eba5a867dc7e81ebabd40af441e0
alvindeo <alvindeoardiansyah@gmail.com>
2025-02-28 18:19:42 +0700

    Add files via upload

---
+++ penugasan/Biodata.java
@@ -0,0 +1,59 @@
+package penugasan; 
+
+// @author Alvin Deo Ardiansyah
+
+// package digunakan agar mempermudah kita untuk menjalankan 
+// program ketika kita memiliki banyak file
+
+public class Biodata{
+    public static void main(String[] args){
+
+        // string adalah tipe data yang digunakan 
+        // untuk menyimpan teks
+
+        // nim
+        String nim;
+        nim = "A11.2023.15072";
+
+        // nama
+        String nama;
+        nama = "Alvin Deo Ardiansyah";
+
+        // alamat
+        String alamat;
+        alamat = "Jl. Purnakarya V no.7";
+
+        // kota
+        String kota;
+        kota = "Semarang";
+
+        // kodepos
+        String kodepos;
+        kodepos = "50519";
+
+        // Telp
+        String telepon;
+        telepon = "085174455921";
+
+        // Hp
+        String hp;
+        hp = "0123456879";
+
+        // Email
+        String email;
+        email = "alvindeoardiansyah@gmail.com";
+
+        //system.out.println adalah perintah untuk menampilkan 
+        // teks ke layar dalam bahasa Java
+
+        // Run
+        System.out.println("NIM : "+nim);
+        System.out.println("Nama : "+nama);
+        System.out.println("Alamat : "+alamat);
+        System.out.println("Kota : "+kota);
+        System.out.println("Kode Pos : "+kodepos);
+        System.out.println("Telp : "+telepon);
+        System.out.println("No Hp : "+hp);
+        System.out.println("Email : "+email);   
+    }
+}
---
+++ penugasan/Menghitung.java
@@ -0,0 +1,52 @@
+package penugasan;
+// @author Alvin Deo Ardiansyah
+
+public class Menghitung{
+    public static void main (String[] args) {
+        //variabel yang dibutuhkan
+        int panjang, lebar, tinggi, jarijari, sisi, alas, luas;
+        panjang = 10;
+        lebar = 5;
+        tinggi = 8;
+        jarijari = 7;
+        sisi = 4;
+        alas = 6;
+        float phi =22/7;
+
+        System.out.println("\n=== RUMUS PERSEGI ===");
+        //rumus luas
+        int LuasPersegi = panjang * lebar;
+        System.out.println("Luas Persegi adalah: " + LuasPersegi);
+
+        //rumus keliling
+        int KelilingPersegi = 4 * panjang;
+        System.out.println("Keliling Persegi adalah: " + KelilingPersegi);
+
+        System.out.println("\n=== RUMUS SEGITIGA ===");
+        //rumus luas
+        float LuasSegitiga = 0.5f * alas * tinggi;
+        System.out.println("Luas Segitiga adalah: " + LuasSegitiga);
+
+        //rumus keliling
+        float KelilingSegitiga = alas + sisi + sisi;
+        System.out.println("Keliling Segitiga adalah: " + KelilingSegitiga);
+
+        System.out.println("\n=== RUMUS LINGKARAN ===");
+        //rumus luas
+        float LuasLingkaran = phi * (jarijari * jarijari);
+        System.out.println("Luas Lingkaran adalah: " + LuasLingkaran);
+
+        //rumus keliling
+        float KelilingLingkaran = 2 * phi * jarijari;
+        System.out.println("Keliling Lingkaran adalah: " + KelilingLingkaran);
+
+        System.out.println("\n=== RUMUS KUBUS ===");
+        //rumus Luas
+        int LuasKubus = 6 * (panjang * panjang);
+        System.out.println("Luas Kubus adalah: " + LuasKubus);
+
+        //rumus keliling
+        int KelilingKubus = 12 * panjang;
+        System.out.println("Keliling Kubus adalah: " + KelilingKubus);
+    }
+}
